285b1f6597e4713fc0e30f904109df284246c77b
henrychen0128 <henrychen0128@gmail.com>
2024-07-03 01:09:00 +0800

    增加證照照片 大頭貼

--- src/app/information-basic/information-basic.component.ts
+++ src/app/information-basic/information-basic.component.ts
@@ -17,7 +17,7 @@ export class InformationBasicComponent
       // this.http.get<any>('https://pokeapi.co/api/v2/ability/?limit=20&offset=20', { observe: 'response' }).subscribe(res => {
         //   this.str =JSON.stringify(res);
       //});
-       let data_json = {"Name":"陳盧宏","age":"31","Gender":"男生","Hobby":[{"content":"打桌球"},{"content":"慢跑"}],"Cer":[{"content":"OCWCD: Oracle Certified Professional Java Programmer"},{"content":"OCPJP: Oracle Certified Professional, Java EE 6 Web Component Developer"},{"content":"DP-100: Microsoft Certified: Azure Data Scientist Associate"}]};
+       let data_json = {"Name":"陳盧宏","age":"31","Gender":"男生","Hobby":[{"content":"打桌球"},{"content":"慢跑"}],"Cer":[{"name":"OCWCD","content":"OCWCD: Oracle Certified Professional Java Programmer","filename":"assets/certification/ocwcd.png"},{"name":"OCPJP","content":"OCPJP: Oracle Certified Professional, Java EE 6 Web Component Developer","filename":"assets/certification/ocpjp.png"},{"name":"DP-100","content":"DP-100: Microsoft Certified: Azure Data Scientist Associate","filename":"assets/certification/dp100.png"}]};
        this.data.push(data_json);
    }
    show_info(infoModal:any,info:any)
@@ -26,4 +26,13 @@ export class InformationBasicComponent
       this.showinfomations = info
       selectModal.show();
     }
+    activeIndex: number | null = null;
+
+  toggleAccordion(index: number) {
+    this.activeIndex = this.activeIndex === index ? null : index;
+  }
+
+  isAccordionOpen(index: number): boolean {
+    return this.activeIndex === index;
+  }
 }
